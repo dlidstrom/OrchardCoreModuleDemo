a32426f6c0d344fea006f6d527bbc91c9d053c39
Daniel Lidstrom <dlidstrom@gmail.com>
2020-06-27 21:32:42 +0200

    adding a widget

--- src/Modules/Workshop.Demo.Module/Migrations/PersonMigrations.cs
+++ src/Modules/Workshop.Demo.Module/Migrations/PersonMigrations.cs
@@ -20,23 +20,38 @@ namespace Workshop.Demo.Module.Migrations
 
         public int Create()
         {
-            contentDefinitionManager.AlterPartDefinition(nameof(PersonPart),
-            part => part.Attachable()
-            .WithField(nameof(PersonPart.Biography), field => field.OfType(nameof(TextField))
-            .WithDisplayName("Biography").WithEditor("TextArea").WithSettings(new TextFieldSettings { Hint = "Person's biography" })));
-            return 1;
-        }
+            contentDefinitionManager.AlterPartDefinition(
+                nameof(PersonPart),
+                part => part.Attachable()
+                    .WithField(nameof(PersonPart.Biography), field => field.OfType(nameof(TextField))
+                    .WithDisplayName("Biography")
+                    .WithEditor("TextArea")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "Person's biography"
+                    })));
 
-        public int UpdateFrom1()
-        {
             SchemaBuilder.CreateMapIndexTable(
                 nameof(PersonPartIndex),
                 table => table
                     .Column<string>(nameof(PersonPartIndex.ContentItemId), x => x.WithLength(26))
                     .Column<int>(nameof(PersonPartIndex.Handedness)));
-            SchemaBuilder.AlterTable(nameof(PersonPartIndex), table => table
-                .CreateIndex($"IDX_{nameof(PersonPartIndex)}_{nameof(PersonPartIndex.Handedness)}", nameof(PersonPartIndex.Handedness)));
-            return 2;
+            SchemaBuilder.AlterTable(
+                nameof(PersonPartIndex),
+                table => table
+                    .CreateIndex(
+                        $"IDX_{nameof(PersonPartIndex)}_{nameof(PersonPartIndex.Handedness)}",
+                        nameof(PersonPartIndex.Handedness)));
+
+            contentDefinitionManager.AlterTypeDefinition("PersonWidget", type => type
+                .DisplayedAs("Person Widget")
+                .Draftable()
+                .Versionable()
+                .WithPart("PersonPage")
+                .WithPart("PersonPart")
+                .Stereotype("Widget")); // categorization for content types
+
+            return 1;
         }
     }
 }
